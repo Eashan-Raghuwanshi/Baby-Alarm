cc45769f2c118b18eeb283eabfe18a0224d3e67b
Eashan-Raghuwanshi <144554613+Eashan-Raghuwanshi@users.noreply.github.com>
2023-09-10 14:16:23 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -13,10 +13,6 @@ function setup()
     video = createCapture(VIDEO);
     video.size(380, 380);
     video.hide(); 
-}
-
-function start()
-{
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
@@ -31,7 +27,7 @@ function gotResult(error, results)
 {
     if(error)
     {
-        console.log(error);12
+        console.log(error);
     }
     console.log(results);
     objects = results;
@@ -41,22 +37,25 @@ function draw()
 {
     image(video, 0, 0, 380, 380);
     
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult)
     for (i=0; i< objects.length; i++)
-    {
-    if(status = "")
-    {
-       document.getElementById("status").innerHTML = "Status : Baby Detected";
-        sound.stop(alarm.mp3);
-    } else(status!= "")
-    {
-        document.getElementById("status").innerHTML = "Status : Baby Detected";
-        sound.play(alarm.mp3);
+    {   
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label = "" + percent + "%", objects[i].x, objects[i].y);
+        noFill();
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
     }
-    }
-    if(objects < 0)
-    {
-        document.getElementById("status").innerHTML = "Status : Baby Not Detected"
-    }  
-    }
+    document.getElementById("status").innerHTML = "Status : Baby Detected";
+    audio.stop(alarm.mp3);
 
-
+    document.getElementById("status").innerHTML = "Status : Baby Detected";
+    audio.play(alarm.mp3);
+}
+}
+if(objects < 0)
+{
+    document.getElementById("status").innerHTML = "Status : Baby Not Detected";
+    audio.play(alarm.mp3);
+}  
